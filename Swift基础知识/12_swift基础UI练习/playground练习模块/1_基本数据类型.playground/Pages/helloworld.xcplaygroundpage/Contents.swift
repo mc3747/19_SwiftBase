d77eace5f999d5805d76e381c26e5177b34cf6dc
马成 <>
2020-05-20 23:15:24 +0800

    playground

---
+++ Swift基础知识/12_swift基础UI练习/playground练习模块/1_基本数据类型.playground/Pages/helloworld.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+import PlaygroundSupport
+
+var str = "Hello, playground"
+let a = 5
+let b:Int
+b = 20
+
+let c = "✅\(str)"
+print(Int.max)
+//: [Next](@next)
