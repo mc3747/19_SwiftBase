ef1e002d9f34e8bb44d33fe0334668f00d6c2565
马成 <mc3747@126.com>
2020-07-13 08:38:32 +0800

    练习

---
+++ SwiftUI-Demo/SwiftUI-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Demo
+//
+//  Created by 马成 on 2020/7/10.
+//  Copyright © 2020 马成. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
