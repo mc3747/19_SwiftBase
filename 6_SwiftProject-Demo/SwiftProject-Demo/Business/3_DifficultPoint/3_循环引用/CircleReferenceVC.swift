cedfb392e58013078663075ca1b3c4eb0d46c343
小马 <mc3747@126.com>
2021-06-23 17:52:49 +0800

    循环引用

--- 6_SwiftProject-Demo/SwiftProject-Demo/Business/3_DifficultPoint/3_循环引用/CircleReferenceVC.swift
+++ 6_SwiftProject-Demo/SwiftProject-Demo/Business/3_DifficultPoint/3_循环引用/CircleReferenceVC.swift
@@ -12,7 +12,7 @@ class CircleReferenceVC: CommonViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //        demo1()
-        demo2()
+        demo7()
     }
     
     //✅：循环引用
@@ -87,8 +87,115 @@ class CircleReferenceVC: CommonViewController {
         john!.card = CreditCard(number: 1234_5678_9012_3456, customer: john!)
     }
     
-//MARK: ✅闭包循环引用
-
-//MARK: ✅定时器循环引用
+    //MARK: ✅闭包循环引用
+    func demo4(){
+        class HTMLElement {
+            
+            let name: String
+            let text: String?
+            
+            lazy var asHTML: () -> String = {
+                //可以使用weak和unowned来修饰，防止循环引用
+                //                [unowned self] in
+                if let text = self.text {
+                    return "<\(self.name)>\(text)</\(self.name)>"
+                } else {
+                    return "<\(self.name) />"
+                }
+            }
+            
+            init(name: String, text: String? = nil) {
+                self.name = name
+                self.text = text
+            }
+            
+            deinit {
+                print("\(name) is being deinitialized")
+            }
+            
+        }
+        let paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+        print(paragraph!.asHTML())
+        
+        
+    }
     
+    //MARK: ✅定时器循环引用
+    //    以下两种方法都会造成循环引用
+    //    var time: Timer?
+    weak var time: Timer?
+    func demo5(){
+        
+        time = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(timePrint), userInfo: nil, repeats: true)
+    }
+    
+    
+    //   解决办法1：使用系统提供Block方法
+    func demo6(){
+        if #available(iOS 10.0, *) {
+            /// iOS 10之后采用`Block`方式解决Timer 循环引用问题
+            time = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { [weak self] (timer) in
+                guard let `self` = self else { return }
+                self.timePrint()
+            })
+        }
+    }
+    
+    //   解决办法2：使用GCD提供的DispatchSource替换Timer
+    var source: DispatchSourceTimer?
+    func demo7(){
+        source = DispatchSource.makeTimerSource(flags: [], queue: .global())
+        source?.schedule(deadline: .now(), repeating: 2)
+        source?.setEventHandler {
+            
+            self.timePrint()
+        }
+        source?.resume()
+    }
+    //    解决办法3：模仿系统提供的closure
+    
+    func demo8(){
+        if #available(iOS 10.0, *) {
+            /// iOS 10之后采用`Block`方式解决Timer 循环引用问题
+            time = Timer.scheduledTimer(withTimeInterval: 2, repeats: true, block: { [weak self] (timer) in
+                guard let `self` = self else { return }
+                self.timePrint()
+            })
+        } else {
+            /// iOS 10之前的解决方案： 模仿系统的`closure` 解决Timer循环引用问题
+            time = Timer.rp_scheduledTimer(timeInterval: 2, repeats: true, closure: { [weak self] (timer) in
+                guard let `self` = self else { return }
+                self.timePrint()
+            })
+        }
+    }
+    
+    //    解决办法4：使用Runtime给对象添加消息处理的方法
+    
+    //    解决办法5：使用NSProxy类作为中间对象
+    
+    @objc func timePrint() {
+        print("打印")
+    }
+    deinit {
+        print("deinit---------------------11111")
+        time?.invalidate()
+        time = nil
+        
+        source?.cancel()
+        source = nil
+    }
 }
+
+extension Timer {
+    class func rp_scheduledTimer(timeInterval ti: TimeInterval, repeats yesOrNo: Bool, closure: @escaping (Timer) -> Void) -> Timer {
+        return self.scheduledTimer(timeInterval: ti, target: self, selector: #selector(RP_TimerHandle(timer:)), userInfo: closure, repeats: yesOrNo)
+    }
+    
+    @objc class func RP_TimerHandle(timer: Timer) {
+        var handleClosure = { }
+        handleClosure = timer.userInfo as! () -> ()
+        handleClosure()
+    }
+}
+
